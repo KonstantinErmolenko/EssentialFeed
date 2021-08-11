d79e2847f132148f488a2750f010ee397b7e4583
Ermolenko.K <19symbols@gmail.com>
2021-08-11 19:37:05 +0300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ермоленко Константин on 11.08.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
