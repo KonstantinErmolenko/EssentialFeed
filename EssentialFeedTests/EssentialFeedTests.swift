118d0db2fdca00739cb0289a4606d5171bcf6b1a
Ermolenko.K <19symbols@gmail.com>
2021-08-13 22:42:15 +0300

    Replace optional params with enum with success/failure cases

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -51,7 +51,7 @@ class EssentialFeedTests: XCTestCase {
         let (sut, client) = makeSUT()
 
         let samples = [199, 201, 300, 400, 500]
-        
+
         samples.enumerated().forEach { index, statusCode in
             var capturedErrors = [RemoteFeedLoader.Error]()
             sut.load() { capturedErrors.append($0) }
@@ -71,18 +71,18 @@ class EssentialFeedTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
 
         func complete(with statusCode: Int, at index: Int = 0) {
@@ -91,8 +91,8 @@ class EssentialFeedTests: XCTestCase {
                 statusCode: statusCode,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
